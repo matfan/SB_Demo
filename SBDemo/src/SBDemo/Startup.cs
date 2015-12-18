cf32c74944b3e67ba7d4163dd643a5f93ccb666c
matfan <mathias.fanden@squeed.com>
2015-12-18 15:01:55 +0100

    Test

--- SBDemo/src/SBDemo/Startup.cs
+++ SBDemo/src/SBDemo/Startup.cs
@@ -15,6 +15,7 @@ namespace SBDemo
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,10 +23,17 @@ namespace SBDemo
         {
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
 
         // Entry point for the application.
